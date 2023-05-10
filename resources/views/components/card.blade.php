be8250c913790cd7784d9351a75ea8805d39b841
Thomas Fazziani <thomasfazziani@gmail.com>
2023-05-10 12:42:38 +0200

    navbar e button

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,7 +1,10 @@
 <div class="card img-fluid card-custom" style="width: 25rem">
     <img class="img-custom" src="./img/{{ $categoryImg }}" alt="" class="card-img-top">
     <div class="card-body">
-        <h4 class="card-title text-center">{{ $categoryCategory }}</h4>
-        <h6> {!! $categoryDescription !!}</h6>
+        <h4 class="card-title text-center mb-3">{{ $categoryCategory }}</h4>
+        <h6 class="text-center mb-3"> {!! $categoryDescription !!}</h6>
+        <div class="d-flex justify-content-center align-items-center">
+        <button class="myButton">Scopri di più</button>
+    </div>
     </div>
 </div>

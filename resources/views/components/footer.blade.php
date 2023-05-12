cf73e7dfeaf770e4a3fbf401f7cef66669c07f27
Gabriele Acquaviva <gabriele.acquaviva@outlook.it>
2023-05-12 18:54:17 +0200

    Sistemato footer momentaneamente

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,10 +1,13 @@
-<footer class="d-flex flex-wrap justify-content-evenly align-items-evenly py-3 my-4 border-top footer">
-  <p class="col-md-4 mb-0 text-dark">© 2023 Company, Inc</p>
-
-  <ul class="nav mx-5 col-md-4">
-    <li class="nav-item"><a href="#" class="nav-link px-2 lead text-dark foo-hov">Home</a></li>
-    <li class="nav-item"><a href="#" class="nav-link px-2 lead text-dark foo-hov">Andrea Palmieri</a></li>
-    <li class="nav-item"><a href="#" class="nav-link px-2 lead text-dark foo-hov">Gabriele Acquaviva</a></li>
-    <li class="nav-item"><a href="{{route('become.revisor')}}" class="nav-link px-2 lead text-dark foo-hov">Lavora con noi</a></li>
-  </ul>
+<footer class=" footer p-3 fw-bold d-flex justify-content-center align-items-center">
+    <p class="col-md-4 mb-0 text-dark text-center">© 2023 Company, Inc</p>
+    <ul class="nav col-md-4 ">
+        <li class="nav-item"><a href="#" class="nav-link lead text-dark foo-hov">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link lead text-dark foo-hov">Andrea Palmieri</a></li>
+        <li class="nav-item"><a href="#" class="nav-link lead text-dark foo-hov">Gabriele Acquaviva</a></li>
+        @if (Auth::user()->is_revisor)
+        @else
+            <li class="nav-item"><a href="{{ route('become.revisor') }}" class="nav-link lead text-dark foo-hov">Lavora
+                    con noi</a></li>
+        @endif
+    </ul>
 </footer>

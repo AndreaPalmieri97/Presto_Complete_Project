072acc688634d61262c30308ee68cff1779c9731
Gabriele Acquaviva <gabriele.acquaviva@outlook.it>
2023-05-16 18:06:32 +0200

    Aggiunta preview image (Non funzionante)

--- app/Http/Livewire/CreateArticle.php
+++ app/Http/Livewire/CreateArticle.php
@@ -4,32 +4,39 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Category;
+use Livewire\WithFileUploads;
 use Illuminate\Support\Facades\Auth;
 
 class CreateArticle extends Component
 {
+    use WithFileUploads;
 
     public $name;
     public $category_id;
     public $category;
     public $description;
     public $price;
+    public $temporary_images;
+    public $image=[];
 
 
-    public function store() {
+
+
+
+    public function store()
+    {
 
         $category = Category::find($this->category);
         $category->articles()->create([
 
-            'name'=>$this->name,
-            'description'=>$this->description,
-            'price'=>$this->price,
+            'name' => $this->name,
+            'description' => $this->description,
+            'price' => $this->price,
             "user_id" => Auth::user()->id,
-            
+
 
         ]);
         return redirect()->route('thanks')->with('message', 'Annuncio inserito correttamente. Grazie per aver utilizzato Presto.it');
-
     }
 
     public function render()
--- resources/views/livewire/create-article.blade.php
+++ resources/views/livewire/create-article.blade.php
@@ -1,8 +1,9 @@
 <div>
     <div class="col-12 col-md-7 mt-3 text-center">
-        <h1 class="my-5 mx-5">{{__('ui.newArticle')}}</h1>
+        <h1 class="my-5 mx-5">{{ __('ui.newArticle') }}</h1>
         <p class="h6 mt-4">Qualsiasi dato verrà acquisito rispettando la privacy come dall'Art n 123.</p>
-        <p class="h6 mt-4">Usufruendo dei servizi offerti da Presto.it accetterai i termini e le condizioni del nostro sito.</p>
+        <p class="h6 mt-4">Usufruendo dei servizi offerti da Presto.it accetterai i termini e le condizioni del nostro
+            sito.</p>
     </div>
     <div class="col-12 col-md-4">
         <form class="my-5" wire:submit.prevent="store">
@@ -11,13 +12,10 @@
                 <label for="name" class="form-label fs-5 fw-bold">Nome</label>
                 <input id="name" type="text" class="form-control" wire:model="name">
             </div>
-            {{-- <div class="mb-3">
-                <label for="img" class="form-label">Image</label>
-                <input type="file" class="form-control" name="img">
-            </div> --}}
             <label for="category" class="form-label mb-3 fs-5 fw-bold">Categoria</label>
-            <select selected id="category" wire:model="category" class="form-select mb-3" aria-label="Default select example">
-               <option value="">Scegli la categoria</option>
+            <select selected id="category" wire:model="category" class="form-select mb-3"
+                aria-label="Default select example">
+                <option value="">Scegli la categoria</option>
                 @foreach ($categories as $category)
                     <option value="{{ $category->id }}">{{ $category->name }}</option>
                 @endforeach
@@ -30,9 +28,25 @@
                 <label for="price" class="form-label fs-5 fw-bold">Prezzo</label>
                 <input id="price" type="number" class="form-control" wire:model="price">
             </div>
+            <div class="mb-3">
+                <label for="img" multiple class="form-label fs-5 fw-bold">Image</label>
+                <input wire:model="temporary_images" type="file" class="shadow form-control" name="img">
+            </div>
+            @if (!empty($imgages))
+                <div class="row">
+                    <div class="col-12">
+                        <p>Anteprima immagine</p>
+                        <div class="row rounded shadow py-4">
+                            @foreach ($images as $key => $image)
+                                <div class="col-3">
+                                    <div class="img-preview" style="background-image:url({{ $image->temporaryUrl() }})"></div>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            @endif
             <button type="submit" class="btn myButton">Inserisci</button>
         </form>
     </div>
 </div>
-
-

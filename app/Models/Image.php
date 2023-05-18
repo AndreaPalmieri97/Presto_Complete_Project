6aac5f4bfcce0efd82eaa945526053888653ee1f
Andrea Palmieri <palmersbari@gmail.com>
2023-05-18 16:08:49 +0200

    Resizing delle immagini completato

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 use App\Models\Article;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Image extends Model
 {
@@ -17,20 +17,24 @@ class Image extends Model
     {
         return $this->belongsTo(Article::class);
     }
-public static function getUrlByFilePath($filePath , $w = null , $h = null){
+
+
+    public static function getUrlByFilePath($filePath , $w = null , $h = null){
+
     if(!$w && !$h){
         return Storage::url($filePath);
     }
 
     $path = dirname($filePath);
     $filename = basename($filePath);
-    $file = "{$path}/crop_{$w}x{$h}_{$filename}";
+    $file = "{$path}/crop_{$w}x{$h}_{$fileName}";
 
     return Storage::url($file);
 }
-public function getUrl($w = null , $h = null)
-{
-return Image::getUrlByFilePath($this->path , $w , $h);
-}
+
+    public function getUrl($w = null , $h = null)
+    {
+        return Image::getUrlByFilePath($this->path , $w , $h);
+    }
 
 }
